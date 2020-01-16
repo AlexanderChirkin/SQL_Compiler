87e25c681bb66b54e9acf72f1cebf97311d8f6f0
AlexanderChirkin <chalex2000@yandex.ru>
2020-01-17 00:33:55 +0300

    Отредактировал справку.

--- cmd_arg_main.py
+++ cmd_arg_main.py
@@ -4,26 +4,12 @@ from typing import List
 
 
 def createParser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-h2', '--help2', action='store_true', default=False)
-    parser.add_argument('-q', '--query', action='store_true', default=False)
-    parser.add_argument('-s', '--show', action='store_true', default=False)
-    parser.add_argument('-r', '--remove', nargs='+')
-    parser.add_argument('-l', '--load', nargs='+', type=argparse.FileType())
-
+    parser = argparse.ArgumentParser(prog='SQL-Compiler', description='commands:', epilog='Use "exit" to exit')
+    parser.add_argument('-l', '--load', nargs='+', type=argparse.FileType(), help='load tables in memory', metavar='path')
+    parser.add_argument('-r', '--remove', nargs='+', help='remove tables from memory', metavar='table_name')
+    parser.add_argument('-s', '--show', action='store_true', default=False, help='show tables in memory')
+    parser.add_argument('-q', '--query', action='store_true', default=False, help='go to write query')
     return parser
-
-def print_help():
-    print("""
-    commands:
-      -h2, --help2                              show this help message
-      -l p1 [p2 ...], --load p1 [p2 ...]        load tables in memory. p1, p2 - pathes to excel-tables
-      -r t1 [t2 ...], --remove t1 [t2 ...]      remove tables from memory. t1, t2 - names of tables
-      -s, --show                                show tables in memory
-      -q, --query                               go to write query
-      
-      Use "exit" to exit
-    """)
 
 
 def print_tables():
@@ -50,11 +36,12 @@ def query():
         text_query += input()
     print('Query: ')
     print(text_query)
+    print('Result: ')
 
 
 def main():
     parser = createParser()
-    print_help()
+    parser.print_help()
     while True:
         print('> ', end='')
         line = input()
@@ -62,10 +49,8 @@ def main():
             break
         try:
             namespace = parser.parse_args(line.split(' '))
-            print(namespace)
-            if namespace.help2:
-                print_help()
-            elif namespace.show:
+            #print(namespace)
+            if namespace.show:
                 print_tables()
             elif namespace.load:
                 load_tables(pathes=namespace.load)
